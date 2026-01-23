9c7436d17164d2058b097054c7a6b61b4dbdfe92
Kariandrrr <educationkarina08@gmail.com>
2026-01-23 12:28:20 +0300

    created user table

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,16 +1,16 @@
 __all__ = (
     "db_helper",
-    "Category",
-    "Post",
-    "Tag",
+    # "Category",
+    # "Post",
+    # "Tag",
     "User",
     "Base",
 )
 
 from .db_helper import db_helper
 
-from .category import Category
-from .post import Post
-from .tag import Tag
+# from .category import Category
+# from .post import Post
+# from .tag import Tag
 from .user import User
 from .base import Base
