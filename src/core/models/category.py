438d907de8e8751ab3d41716fc065c82eff60ce4
Kariandrrr <educationkarina08@gmail.com>
2026-01-23 16:00:17 +0300

    Update category.py

--- src/core/models/category.py
+++ src/core/models/category.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 
 from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, relationship
@@ -8,11 +8,15 @@ from .base import Base
 from .mixins.created_at import CreatedAtMixin
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from . import Post
+
 
 class Category(Base, IntIdPkMixin, CreatedAtMixin):
     name: Mapped[str] = mapped_column(String(100), unique=True)
     slug: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     description: Mapped[str] = mapped_column(Text, nullable=True)
 
-
-# posts: Mapped[List["Post"]] = relationship(secondary="post_category" ,back_populates="categories")
+    posts: Mapped[List["Post"]] = relationship(
+        secondary="post_category", back_populates="categories"
+    )
