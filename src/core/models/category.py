1c57b5baceca790003e956f02e27182ec89b2301
Kariandrrr <educationkarina08@gmail.com>
2026-01-23 12:26:06 +0300

    ooooooppps, had to drop everything to start again

--- src/core/models/category.py
+++ src/core/models/category.py
@@ -1,17 +1,18 @@
-# from typing import List
-#
-# from sqlalchemy import String, Text
-# from sqlalchemy.orm import Mapped, relationship
-# from sqlalchemy.orm import mapped_column
-#
-# from .base import Base
-# from .mixins.created_at import CreatedAtMixin
-# from .mixins.int_id_pk import IntIdPkMixin
-#
-#
-# class Category(Base, IntIdPkMixin, CreatedAtMixin):
-#     name: Mapped[str] = mapped_column(String(100), unique=True)
-#     slug: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-#     description: Mapped[str] = mapped_column(Text, nullable=True)
-#
-#     # posts: Mapped[List["Post"]] = relationship(secondary="post_category" ,back_populates="categories")
+from typing import List
+
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+from .mixins.created_at import CreatedAtMixin
+from .mixins.int_id_pk import IntIdPkMixin
+
+
+class Category(Base, IntIdPkMixin, CreatedAtMixin):
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    slug: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+
+
+# posts: Mapped[List["Post"]] = relationship(secondary="post_category" ,back_populates="categories")
