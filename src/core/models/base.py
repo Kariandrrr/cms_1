9d702d7bd28e1d538f9a8ca339ed8c2da7335adc
Kariandrrr <educationkarina08@gmail.com>
2026-03-16 08:07:53 +0300

    delete comments

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -4,15 +4,12 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy.orm import Mapped, mapped_column
 
-# Проверяем, запущен ли скрипт через Alembic
 is_alembic_running = "alembic" in sys.modules or "alembic" in sys.argv[0]
 
 if is_alembic_running:
-    # Для Alembic используем абсолютные импорты
     from core.config import settings
     from utils import camel_case_to_snake_case
 else:
-    # Для обычного запуска сервера используем относительные импорты
     from ..config import settings
     from ...utils import camel_case_to_snake_case
 
